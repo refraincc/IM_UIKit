5bfccf131d4e8947f92c2ee8816316e77041b06b
CC <317642637@qq.com>
2017-02-14 00:16:21 +0800

    添加文件夹

--- IM_UIKit/IM/IM/Classes/Sections/Login/Manager/RCLoginManager.h
+++ IM_UIKit/IM/IM/Classes/Sections/Login/Manager/RCLoginManager.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  RCLoginManager.h
 //  IM
 //
 //  Created by PushMeBaby on 13/02/2017.
 //  Copyright © 2017 refrainC. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
-
+@interface RCLoginManager : NSObject
 
 @end
-
---
+++ IM_UIKit/IM/IM/Classes/Sections/Login/Manager/RCLoginManager.m
@@ -0,0 +1,13 @@
+//
+//  RCLoginManager.m
+//  IM
+//
+//  Created by PushMeBaby on 13/02/2017.
+//  Copyright © 2017 refrainC. All rights reserved.
+//
+
+#import "RCLoginManager.h"
+
+@implementation RCLoginManager
+
+@end
